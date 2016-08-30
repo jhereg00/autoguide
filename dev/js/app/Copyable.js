bf2e4922b2101a84af5b28a7ecb470e0a4eb553f
Martin, Glenn (DET-CEW) <Glenn.Martin@c-e.com>
2016-08-30 13:15:29 -0400

    added copyable

---
+++ dev/js/app/Copyable.js
@@ -0,0 +1,59 @@
+/***
+ *  Copyable
+ *
+ *  Makes an element clickable, copying a string to the user's clipboard.
+ *
+ *  @param {DOMElement} element
+ *  @param {string} string to copy
+ *
+ *  @method {DOMElement} makeInput() - internal function to make the input from
+ *    which the string will be copied.
+ *  @method copy() - copies string to clipboard. Listener is automatically added,
+ *    so you shouldn't need to manually call this.
+ */
+// requirements
+var forEach = require('lib/util/forEach');
+
+// settings
+
+// the class
+var Copyable = function (element, string) {
+  this.element = element;
+  this.string = string;
+
+  if (document.execCommand) {
+    document.body.classList.add('copyable-enabled');
+    this.makeInput();
+    var _this = this;
+    this.element.addEventListener('click', function (e) {
+      e.preventDefault();
+      _this.copy();
+    });
+  }
+}
+Copyable.prototype = {
+  makeInput: function () {
+    this.input = document.createElement('input');
+    this.input.classList.add('visuallyhidden');
+    this.input.value = this.string;
+    return this.input;
+  },
+  copy: function () {
+    this.element.parentNode.insertBefore(this.input, this.element);
+    this.input.select();
+    try {
+      document.execCommand('copy');
+    } catch (err) {};
+    this.input.blur();
+    this.input.remove();
+  }
+}
+
+// auto-generate
+var copyables = [];
+var copyableEls = document.querySelectorAll('[data-copy]');
+forEach(copyableEls, function (el) {
+  copyables.push(new Copyable (el, el.getAttribute('data-copy')));
+});
+
+module.exports = Copyable;
--- dev/js/autoguide.js
+++ dev/js/autoguide.js
@@ -9,3 +9,4 @@ require('app/HtmlSample').makeAll();
 require('app/controls');
 require('app/Tray');
 require('app/hashchange');
+require('app/Copyable');
